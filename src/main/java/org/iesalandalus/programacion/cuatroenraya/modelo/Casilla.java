878fe0c76a66c82a901a83aa526cf8f33a8f735f
CJ <CJ@DESKTOP-OS2TG00>
2021-01-08 19:46:40 +0100

    He corregido los errores que habia

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -1,37 +1,48 @@
 package org.iesalandalus.programacion.cuatroenraya.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Casilla {
 	
 	private Ficha ficha;
 	
 	public Casilla() {
 		super();
-		this.ficha = null;
+		ficha = null;
 	}
 
 	public Ficha getFicha() {
-		return ficha;
+		return this.ficha;
 	}
 
-	public void setFicha(Ficha ficha) {
-		this.ficha = ficha;
+	public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+		if (ficha == null) {
+			throw new NullPointerException("ERROR: No se puede poner una ficha nula.");
+		}
+		
+		if(this.ficha != null) {
+			throw new OperationNotSupportedException("ERROR: Ya contengo una ficha.");
+		} else {
+			this.ficha = ficha;
+		}
 	}
 	
 	public boolean estaOcupada() {
-		if(ficha != null) {
+		if(getFicha() != null) {
 			return true;
-		} else {
+		} else { 
 			return false;
 		}
 	}
 
 	public String toString() {
-		
-		if(estaOcupada()) {
-			return String.format("%.1s", ficha);
-		} else {
-			return " ";
-		}
+
+				if(getFicha() != null) {
+					return String.format("%.1s", ficha);
+				} else {
+					return " ";
+				}
+
 	}
 
 }
